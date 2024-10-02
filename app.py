b1bb0429d06f0e6a0ee1db443c689f97db1b041b
arthurrodrigo2 <arthurrodrigo2@gmail.com>
2024-10-02 20:56:39 -0300

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,10 @@
+# Variável
+nome_completo = input("Informe seu nome completo: ")
+
+# separa os nomes em uma lista
+nome_completo_lista = nome_completo.split(" ")
+
+# capitaliza os nomes e exibe o nome completo na lista
+print("Nome completo: ", end="")
+for nome in nome_completo_lista:
+    print(nome.capitalize(), end=" ")
